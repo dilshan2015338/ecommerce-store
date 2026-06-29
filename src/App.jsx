0159fbd9015c2682a9d9cad5c9ce46147ae775a1
dilshan <dilshanm@icanim.com>
2026-06-29 14:55:32 +0530

    03-product-list-mock: Add ProductCard and ProductsPage with mock data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import HomePage from './pages/HomePage';
+import ProductsPage from "./pages/ProductsPage.jsx";
 
 function App() {
     return (
@@ -12,6 +13,8 @@ function App() {
                 <main className="flex-1">
                     <Routes>
                         <Route path="/" element={<HomePage />} />
+                        <Route path="/products" element={<ProductsPage />} />
+                        <Route path="/product/:id" element={<div>Product Detail Coming Soon</div>} />
                         {/* More routes will be added */}
                     </Routes>
                 </main>
